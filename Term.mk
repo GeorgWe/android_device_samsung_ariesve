05e8cd755648b0874174f4540b60e069424e8527
david <davidteri91@gmail.com>
2013-06-29 08:32:29 +0200

    Terminal Emulator and Update gitignore

---
+++ Term.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/Term.apk:system/app/Term.apk \
+    $(LOCAL_PATH)/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
