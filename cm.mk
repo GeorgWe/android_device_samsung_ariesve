01d889dd4ac8fbe254318cbe09ec9f4d830e0a22
david <davidteri91@gmail.com>
2013-06-29 08:17:36 +0200

    Terminal Emulator Support

--- cm.mk
+++ cm.mk
@@ -10,6 +10,8 @@ $(call inherit-product, $(LOCAL_PATH)/full_ariesve.mk)
 # Release name
 PRODUCT_RELEASE_NAME := ariesve
 
+$(call inherit-product, $(LOCAL_PATH)/Term.mk)
+
 # Setup device configuration
 PRODUCT_NAME := cm_ariesve
 PRODUCT_DEVICE := ariesve
@@ -18,4 +20,3 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I9001
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=samsung/GT-I9001/GT-I9001:2.3.6/GINGERBREAD/XXKQN:user/release-keys PRIVATE_BUILD_DESC="GT-I9001-user 2.3.6 GINGERBREAD XXKQN release-keys"
-
