b67fd3a1bc0179827ade0e8063937dd30d42ef93
david <davidteri91@gmail.com>
2013-07-07 16:29:27 +0200

    fix typo

--- liblight/lights.c
+++ liblight/lights.c
@@ -191,7 +191,7 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .version_major = 1,
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "AriesVE Light Module",
+    .name = "AriesVE Lights Module",
     .author = "The CyanogenMod Project",
     .methods = &lights_module_methods,
 };
